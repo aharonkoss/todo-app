8eaecf62745f16ef5eeacefd40a57b0c3fef10c5
Aharon Koss <aharonkoss@gmail.com>
2024-09-27 10:48:23 -0700

    jinit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clientId: '493dfb9b38520e0d', // Your Pipedrive App OAuth 2 Client ID
+    clientSecret: 'ee455a34a084187f6491f4695b83a813483121e1', // Your Pipedrive App OAuth 2 Client Secret
+    redirectUri: 'https://localhost:3000/callback', // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
+    surfaceJwt: 'surfaceJWTSecret', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Surface request
+    embeddedActionJwt: 'embeddedActionJWTSecret', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Embedded action request
+}
